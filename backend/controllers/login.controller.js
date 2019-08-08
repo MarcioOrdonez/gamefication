1a76a774a53967f73450381920b9b18b8785c982
Marcio Ordonez <mordonez@img.com.br>
2019-08-08 16:34:10 -0300

    Token

--- backend/controllers/login.controller.js
+++ backend/controllers/login.controller.js
@@ -1,18 +1,25 @@
 const Register = require('../models/user.model');
 const Bcrypt = require('bcrypt');
+require("dotenv-safe").config();
+var jwt = require('jsonwebtoken');
 
-
-exports.login = async  (req, res) => {
+exports.login = async (req, res) => {
     try {
-        var user = await Register.findOne({email: req.query.email}).exec();
-        if(!user){
+        var user = await Register.findOne({ email: req.body.email }).exec();
+        if (!user) {
             return res.status(400).send({ message: "The username does not exist" });
         }
-        if(!Bcrypt.compareSync(req.query.password, user.password)){
+        if (!Bcrypt.compareSync(req.body.password, user.password)) {
             return res.status(400).send({ message: "The password is invalid" });
         }
         res.send({ message: "The username and password combination is correct!" });
+        const id = user.id;
+        var token = jwt.sign({ id }, process.env.SECRET, {
+            expiresIn: 300 // expires in 5min
+        });
+        res.status(200).send({ auth: true, token: token });
         console.log('deu certo');
-    } catch (error){
-        res.status(500).send(error);
-    }};
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    };
